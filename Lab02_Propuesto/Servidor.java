7744a5501f194c0e20570d21987546c4e7b22058
Angelote77 <83073992+Angelote77@users.noreply.github.com>
2024-05-12 01:49:53 -0500

    creación de la clase servidor

--- Lab02_Propuesto/Servidor.java
+++ Lab02_Propuesto/Servidor.java
@@ -1,5 +1,28 @@
 package Lab02_Propuesto;
 
-public class Servidor {
+import java.io.*;
+import java.net.*;
+import java.time.Instant;
 
+public class Servidor {
+    public static void main(String[] args) {
+        try {
+            ServerSocket servidor = new ServerSocket(5000);
+            System.out.println("Servidor listo para recibir conexiones...");
+            
+            while (true) {
+                Socket socket = servidor.accept();
+                System.out.println("Cliente conectado desde " + socket.getInetAddress());
+                Instant serverTime = Instant.now();
+                
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                out.writeObject(serverTime);
+                
+                out.close();
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
