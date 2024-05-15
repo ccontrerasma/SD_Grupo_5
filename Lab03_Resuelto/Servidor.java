b88ff93d37326591660901d04980e6f26d24d595
Angelote77 <83073992+Angelote77@users.noreply.github.com>
2024-05-15 03:43:06 -0500

    cambios en el código de servidor

--- Lab03_Resuelto/Servidor.java
+++ Lab03_Resuelto/Servidor.java
@@ -1,46 +1,30 @@
-package Lab02_Propuesto;
+package Lab03_Resuelto;
 
 import java.io.*;
 import java.net.*;
-import java.time.Instant;
 
 public class Servidor {
-    public static void main(String[] args) {
-    	// Declaramos el ServerSocket fuera del bloque try-catch para asegurarnos de que pueda ser cerrado en cualquier caso.
-    	ServerSocket servidor = null; 
-        try {
-            // Crea un servidor en el puerto 5000
-            servidor = new ServerSocket(5000);
-            System.out.println("Servidor listo para recibir conexiones...");
-            
-            while (true) {
-                // Espera y acepta conexiones de clientes
-                Socket socket = servidor.accept();
-                // Muestra la dirección IP del cliente conectado
-                System.out.println("Cliente conectado desde " + socket.getInetAddress());
-                // Obtiene el tiempo actual del servidor
-                Instant serverTime = Instant.now();
-                
-                // Crea un flujo de salida para enviar el tiempo al cliente
-                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-                // Envía el tiempo actual al cliente
-                out.writeObject(serverTime);
-                
-                // Cierra el flujo de salida y el socket para el cliente actual
-                out.close();
-                socket.close();
-            }
-        } catch (IOException e) {
-            // Muestra cualquier excepción ocurrida durante la ejecución
-            e.printStackTrace();
-        } finally {
-            try {
-                if (servidor != null) {
-                    servidor.close(); // Cerramos el ServerSocket
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	static final int PUERTO = 5000;
+
+	public Servidor() {
+		try {
+			ServerSocket skServidor = new ServerSocket(PUERTO);
+			System.out.println("Escucho el puerto " + PUERTO);
+			for (int numCli = 0; numCli < 3; numCli++) {
+				Socket skCliente = skServidor.accept();
+				System.out.println("Sirvo al cliente " + numCli);
+				OutputStream aux = skCliente.getOutputStream();
+				DataOutputStream flujo = new DataOutputStream(aux);
+				flujo.writeUTF("Hola cliente " + numCli);
+				skCliente.close();
+			}
+			System.out.println("Demasiados clientes por hoy");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void main(String[] arg) {
+		new Servidor();
+	}
 }
