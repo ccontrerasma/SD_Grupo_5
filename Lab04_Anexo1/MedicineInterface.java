ae1fd8883e2bb65a246e6a41a78156053dbb9bca
Pedro <psanchezm@unsa.edu.pe>
2024-05-23 09:01:00 -0500

    Se creo la carpeta donde se sube el anexo 1 del lab4, se subieron los archivos del anexo 1.

---
+++ Lab04_Anexo1/MedicineInterface.java
@@ -0,0 +1,9 @@
+package SD_Grupo_5.Lab04_Anexo1;
+
+
+import java.rmi.Remote;
+public interface MedicineInterface extends Remote {
+public Medicine getMedicine( int amount ) throws Exception;
+public int getStock() throws Exception;
+public String print() throws Exception;
+}
---
+++ Lab04_Anexo1/StockException.java
@@ -0,0 +1,7 @@
+package SD_Grupo_5.Lab04_Anexo1;
+
+public class StockException extends Exception {
+    public StockException(String msg) {
+        super(msg);
+    }
+}
