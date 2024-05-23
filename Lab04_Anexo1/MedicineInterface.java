dec59ef305ffe8b711c7e8f8cb7b0d27e782dab8
Pedro <psanchezm@unsa.edu.pe>
2024-05-23 09:06:29 -0500

    Analizando StockInterface, se agregaron comentarios y se dio formato a las clases.

--- Lab04_Anexo1/MedicineInterface.java
+++ Lab04_Anexo1/MedicineInterface.java
@@ -1,9 +1,11 @@
 package SD_Grupo_5.Lab04_Anexo1;
 
-
 import java.rmi.Remote;
+
 public interface MedicineInterface extends Remote {
-public Medicine getMedicine( int amount ) throws Exception;
-public int getStock() throws Exception;
-public String print() throws Exception;
+    public Medicine getMedicine(int amount) throws Exception;
+
+    public int getStock() throws Exception;
+
+    public String print() throws Exception;
 }
--- Lab04_Anexo1/StockInterface.java
+++ Lab04_Anexo1/StockInterface.java
@@ -4,10 +4,14 @@ import java.rmi.Remote;
 import java.util.ArrayList;
 import java.util.*;
 
+//Interface define las caracteristicas ofrecidas por el servicio.
 public interface StockInterface extends Remote {
+    //hash que relaciona el producto con su stock.
     public HashMap<String, MedicineInterface> getStockProducts() throws Exception;
 
+    //Metodo para agregar productos.
     public void addMedicine(String name, float price, int stock) throws Exception;
 
+    //Metodo para comprar medicinas.
     public MedicineInterface buyMedicine(String name, int amount) throws Exception;
 }
